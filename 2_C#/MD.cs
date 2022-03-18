0f220325bf8d4e2a3d2592c10d25b87e2c16ef93
Ania W <100371061+AnaShiro@users.noreply.github.com>
2022-03-18 14:00:10 +0100

    sprawdzanie grafu

--- 2_C#/MD.cs
+++ 2_C#/MD.cs
@@ -89,5 +89,88 @@ for (int i = 0; i < maxPolaczen(iloscWierzcholkow); i++) {
 }
 
 int [,] istnieniePolaczen = new int[iloscWierzcholkow, iloscWierzcholkow]; 
+//-------------------------------------------------------------------------------------------START
+uzupelnianieIstniejacychPolaczen(iloscWierzcholkow, istnieniePolaczen, polaczenia);
+bool[] sprawdzoneWierzcholki = new bool[iloscWierzcholkow];
+for(int i = 0; i < sprawdzoneWierzcholki.Length; i++) {
+    sprawdzoneWierzcholki[i] = false;
+}
+int[,] wierzcholkiDoSprawdzenia = new int[2, iloscWierzcholkow*iloscWierzcholkow];
+for( int i = 0;i < 2;i++) {
+    for (int j = 0; j < iloscWierzcholkow*iloscWierzcholkow; j++) {
+        wierzcholkiDoSprawdzenia[i,j] = -1;
+    }
+}
+bool warunekZakonczenia = true;
+int ileWierzcholkowWystapilo = 0;
+int gdzieWpisacNowe = 0; 
+int ktoraSprawdzamy = 0;
+int kolumnaCzyWiersz = 0; //0 - kolumna, 1 - wiersz
+for(int i = 0; i < iloscWierzcholkow; i++) {
+    if(istnieniePolaczen[0,i] == 1) {
+        wierzcholkiDoSprawdzenia[0, gdzieWpisacNowe] = 0;
+        wierzcholkiDoSprawdzenia[1, gdzieWpisacNowe] = i;
+        gdzieWpisacNowe++;
+    }
+}
+for(int i = 0; i < 2; i++) {
+    for (int j = 0; j < iloscWierzcholkow * iloscWierzcholkow; j++) {
+        if (wierzcholkiDoSprawdzenia[i, j] != -1) {
+            sprawdzoneWierzcholki[wierzcholkiDoSprawdzenia[i, j]] = true;
+        }
+    }
+}
+
+do {
+    if (kolumnaCzyWiersz == 0) {
+        for (int i = 0; i < iloscWierzcholkow; i++) { 
+            if(istnieniePolaczen[i, ktoraSprawdzamy] == 1) { 
+                wierzcholkiDoSprawdzenia[0, gdzieWpisacNowe] = 0;
+                wierzcholkiDoSprawdzenia[1, gdzieWpisacNowe] = i;
+                gdzieWpisacNowe++;
+            }
+        }
+        kolumnaCzyWiersz = 1; 
+    }
+    else {
+        for (int i = 0; i < iloscWierzcholkow; i++) {
+            if (istnieniePolaczen[ktoraSprawdzamy, i] == 1) { //przy nieprawidlowym twierdzi, że wychodze poza zakres
+                wierzcholkiDoSprawdzenia[0, gdzieWpisacNowe] = 0;
+                wierzcholkiDoSprawdzenia[1, gdzieWpisacNowe] = i;
+                gdzieWpisacNowe++;
+            }
+        }
+        kolumnaCzyWiersz = 0;
+    }
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < iloscWierzcholkow * iloscWierzcholkow; j++) {
+            if (wierzcholkiDoSprawdzenia[i, j] != -1) {
+                sprawdzoneWierzcholki[wierzcholkiDoSprawdzenia[i, j]] = true;
+            }
+        }
+    }
+    ktoraSprawdzamy++;
+    for (int i = 0; i < iloscWierzcholkow; i++) {
+        if (sprawdzoneWierzcholki[i] == true) {
+            ileWierzcholkowWystapilo++;
+        }
+    }
+    if (ileWierzcholkowWystapilo == iloscWierzcholkow) {
+        warunekZakonczenia = false;
+    }
+    else {
+        ileWierzcholkowWystapilo = 0;
+    }
+    if(wierzcholkiDoSprawdzenia[0,ktoraSprawdzamy] == -1) {
+        warunekZakonczenia = false;
+    }
+} while(warunekZakonczenia==true);
+if(ileWierzcholkowWystapilo==iloscWierzcholkow) {
+    Console.WriteLine("jest git");
+}
+else {
+    Console.WriteLine("graf is zepsuted");
+}
+//--------------------------------------------------------------------------------------KONIEC
 uzupelnianieIstniejacychPolaczen(iloscWierzcholkow, istnieniePolaczen, polaczenia);
 Console.WriteLine($"ilość trójkątów = {zliczanieTrojkatow(istnieniePolaczen, iloscWierzcholkow)}");
