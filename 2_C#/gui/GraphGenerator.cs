5e59b48d231d93e99fa4c85a1767de6a72086db5
Ania W <100371061+AnaShiro@users.noreply.github.com>
2022-03-21 14:30:03 +0100

    poprawa instnieniePolaczen przed zlicz.trojki

--- 2_C#/gui/GraphGenerator.cs
+++ 2_C#/gui/GraphGenerator.cs
@@ -193,7 +193,7 @@
 
             return polaczenia;
         }
-
+        uzupelnianieIstniejacychPolaczen(iloscWierzcholkow, istnieniePolaczen, polaczenia);
         public int liczbaTrojkatow() => zliczanieTrojkatow(istnieniePolaczen, iloscWierzcholkow);
     }
 }
